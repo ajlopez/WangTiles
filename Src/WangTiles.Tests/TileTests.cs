705ed6ce74772b3d7ad503108ad4039a49c709ef
ajlopez <webmaster@ajlopez.com>
2016-03-27 17:40:12 -0300

    Create tile first test

---
+++ Src/WangTiles.Tests/TileTests.cs
@@ -0,0 +1,23 @@
+﻿namespace WangTiles.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TileTests
+    {
+        [TestMethod]
+        public void CreateTileWithColors()
+        {
+            var tile = new Tile(0, 1, 2, 3);
+
+            Assert.AreEqual(0, tile.East);
+            Assert.AreEqual(1, tile.North);
+            Assert.AreEqual(2, tile.West);
+            Assert.AreEqual(3, tile.South);
+        }
+    }
+}
---
+++ Src/WangTiles/Tile.cs
@@ -0,0 +1,28 @@
+﻿namespace WangTiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tile
+    {
+        private byte[] colors = new byte[4];
+
+        public Tile(short east, short north, short west, short south)
+        {
+            colors[0] = (byte)east;
+            colors[1] = (byte)north;
+            colors[2] = (byte)west;
+            colors[3] = (byte)south;
+        }
+
+        public byte East { get { return this.colors[0]; } }
+
+        public byte North { get { return this.colors[1]; } }
+
+        public byte West { get { return this.colors[2]; } }
+
+        public byte South { get { return this.colors[3]; } }
+    }
+}
