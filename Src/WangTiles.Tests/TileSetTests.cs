8a77831ebff6c9fbf41c9349fccc1f4baa415047
ajlopez <webmaster@ajlopez.com>
2016-03-31 07:51:59 -0300

    TileSet first creation test

---
+++ Src/WangTiles.Tests/TileSetTests.cs
@@ -0,0 +1,26 @@
+﻿namespace WangTiles.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TileSetTests
+    {
+        [TestMethod]
+        public void CreateWithTiles()
+        {
+            Tile tile1 = new Tile(0, 0, 0, 0);
+            Tile tile2 = new Tile(0, 1, 0, 1);
+
+            TileSet set = new TileSet(new Tile[] { tile1, tile2 });
+
+            Assert.IsNotNull(set.Tiles);
+            Assert.AreEqual(2, set.Tiles.Count());
+            Assert.IsTrue(set.Tiles.Contains(tile1));
+            Assert.IsTrue(set.Tiles.Contains(tile2));
+        }
+    }
+}
---
+++ Src/WangTiles/TileSet.cs
@@ -0,0 +1,20 @@
+﻿namespace WangTiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TileSet
+    {
+        private IList<Tile> tiles;
+
+        public TileSet(IEnumerable<Tile> tiles)
+        {
+            this.tiles = new List<Tile>(tiles);
+        }
+
+        public IEnumerable<Tile> Tiles { get { return this.tiles; } }
+    }
+}
+
