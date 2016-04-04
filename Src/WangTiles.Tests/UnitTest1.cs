39edf60b91127da7373cd0a876b4f0f4e5fc2d17
ajlopez <webmaster@ajlopez.com>
2016-04-04 08:29:39 -0300

    Create MultiTile from one tile

---
+++ Src/WangTiles.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace WangTiles.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateMultiTileFromTile()
+        {
+            var mtile = new MultiTile(new Tile[] { new Tile(0, 1, 2, 3) });
+
+            Assert.AreEqual(0x01, mtile.East);
+            Assert.AreEqual(0x02, mtile.North);
+            Assert.AreEqual(0x04, mtile.West);
+            Assert.AreEqual(0x08, mtile.South);
+        }
+    }
+}
---
+++ Src/WangTiles/MultiTile.cs
@@ -0,0 +1,38 @@
+﻿namespace WangTiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MultiTile
+    {
+        private short[] colors;
+
+        public MultiTile(IEnumerable<Tile> tiles)
+        {
+            this.colors = new short[4];
+
+            foreach (var tile in tiles)
+            {
+                this.colors[0] |= ColorToBit(tile.East);
+                this.colors[1] |= ColorToBit(tile.North);
+                this.colors[2] |= ColorToBit(tile.West);
+                this.colors[3] |= ColorToBit(tile.South);
+            }
+        }
+
+        public short East { get { return this.colors[0]; } }
+
+        public short North { get { return this.colors[1]; } }
+
+        public short West { get { return this.colors[2]; } }
+
+        public short South { get { return this.colors[3]; } }
+
+        private short ColorToBit(byte color)
+        {
+            return (short)(1 << color);
+        }
+    }
+}
