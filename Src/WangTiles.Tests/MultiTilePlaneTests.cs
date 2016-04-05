268bf273cc62ad27dc83ffe6dcc5e312af6ee923
ajlopez <webmaster@ajlopez.com>
2016-04-05 07:37:12 -0300

    MultiTilePlane first tests

---
+++ Src/WangTiles.Tests/MultiTilePlaneTests.cs
@@ -0,0 +1,37 @@
+﻿namespace WangTiles.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MultiTilePlaneTests
+    {
+        [TestMethod]
+        public void CreateEmptyPlane()
+        {
+            var plane = new MultiTilePlane();
+
+            Assert.IsNull(plane.Get(0, 0));
+        }
+
+        [TestMethod]
+        public void SetAndGetMultiTile()
+        {
+            var mtile = new MultiTile(new short[] { 1, 2, 4, 8 });
+            var plane = new MultiTilePlane();
+
+            plane.Set(0, 0, mtile);
+
+            var result = plane.Get(0, 0);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.East);
+            Assert.AreEqual(2, result.North);
+            Assert.AreEqual(4, result.West);
+            Assert.AreEqual(8, result.South);
+        }
+    }
+}
---
+++ Src/WangTiles/MultiTilePlane.cs
@@ -0,0 +1,29 @@
+﻿namespace WangTiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MultiTilePlane
+    {
+        private int size;
+        private MultiTile[,] plane;
+
+        public MultiTilePlane()
+        {
+            this.size = 5;
+            this.plane = new MultiTile[11, 11];
+        }
+
+        public MultiTile Get(int x, int y)
+        {
+            return this.plane[x + this.size, y + this.size];
+        }
+
+        public void Set(int x, int y, MultiTile mtile)
+        {
+            this.plane[x + this.size, y + this.size] = mtile;
+        }
+    }
+}
