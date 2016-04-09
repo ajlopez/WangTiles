073baa18eae52d64e214fc2124cb49030bc0e888
ajlopez <webmaster@ajlopez.com>
2016-04-09 11:08:38 -0300

    MultiTilePlane ApplyAction

--- Src/WangTiles.Tests/MultiTilePlaneTests.cs
+++ Src/WangTiles.Tests/MultiTilePlaneTests.cs
@@ -50,5 +50,108 @@
             Assert.AreEqual(4, result.West);
             Assert.AreEqual(8, result.South);
         }
+
+        [TestMethod]
+        public void ApplyActionWithoutChanges()
+        {
+            Tile tile1 = new Tile(0, 1, 2, 3);
+            Tile tile2 = new Tile(1, 2, 3, 4);
+            TileSet tset = new TileSet(new Tile[] { tile1, tile2 });
+            MultiTile mtile = new MultiTile(tset);
+
+            var plane = new MultiTilePlane(tset);
+
+            var result = plane.Apply(new MultiTileAction(mtile.East, Direction.East, 0, 0));
+            Assert.IsNull(result);
+
+            result = plane.Apply(new MultiTileAction(mtile.North, Direction.North, 0, 0));
+            Assert.IsNull(result);
+
+            result = plane.Apply(new MultiTileAction(mtile.West, Direction.West, 0, 0));
+            Assert.IsNull(result);
+
+            result = plane.Apply(new MultiTileAction(mtile.South, Direction.South, 0, 0));
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ApplyActionToEast()
+        {
+            Tile tile1 = new Tile(0, 1, 2, 3);
+            Tile tile2 = new Tile(1, 2, 3, 4);
+            TileSet tset = new TileSet(new Tile[] { tile1, tile2 });
+            MultiTile mtile = new MultiTile(tset);
+
+            var plane = new MultiTilePlane(tset);
+
+            var result = plane.Apply(new MultiTileAction(1, Direction.East, 0, 0));
+            
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count());
+            Assert.IsTrue(result.Contains(new MultiTileAction(1, Direction.West, 1, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(2, Direction.North, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(4, Direction.West, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(8, Direction.South, 0, 0)));
+        }
+
+        [TestMethod]
+        public void ApplyActionToNorth()
+        {
+            Tile tile1 = new Tile(0, 1, 2, 3);
+            Tile tile2 = new Tile(1, 2, 3, 4);
+            TileSet tset = new TileSet(new Tile[] { tile1, tile2 });
+            MultiTile mtile = new MultiTile(tset);
+
+            var plane = new MultiTilePlane(tset);
+
+            var result = plane.Apply(new MultiTileAction(2, Direction.North, 0, 0));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count());
+            Assert.IsTrue(result.Contains(new MultiTileAction(2, Direction.South, 0, 1)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(1, Direction.East, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(4, Direction.West, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(8, Direction.South, 0, 0)));
+        }
+
+        [TestMethod]
+        public void ApplyActionToWest()
+        {
+            Tile tile1 = new Tile(0, 1, 2, 3);
+            Tile tile2 = new Tile(1, 2, 3, 4);
+            TileSet tset = new TileSet(new Tile[] { tile1, tile2 });
+            MultiTile mtile = new MultiTile(tset);
+
+            var plane = new MultiTilePlane(tset);
+
+            var result = plane.Apply(new MultiTileAction(4, Direction.West, 0, 0));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count());
+            Assert.IsTrue(result.Contains(new MultiTileAction(4, Direction.East, -1, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(1, Direction.East, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(2, Direction.North, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(8, Direction.South, 0, 0)));
+        }
+
+        [TestMethod]
+        public void ApplyActionToSouth()
+        {
+            Tile tile1 = new Tile(0, 1, 2, 3);
+            Tile tile2 = new Tile(1, 2, 3, 4);
+            TileSet tset = new TileSet(new Tile[] { tile1, tile2 });
+            MultiTile mtile = new MultiTile(tset);
+
+            var plane = new MultiTilePlane(tset);
+
+            var result = plane.Apply(new MultiTileAction(8, Direction.South, 0, 0));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count());
+            Assert.IsTrue(result.Contains(new MultiTileAction(8, Direction.North, 0, -1)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(1, Direction.East, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(2, Direction.North, 0, 0)));
+            Assert.IsTrue(result.Contains(new MultiTileAction(4, Direction.West, 0, 0)));
+        }
     }
 }
--- Src/WangTiles/MultiTilePlane.cs
+++ Src/WangTiles/MultiTilePlane.cs
@@ -9,6 +9,7 @@
     {
         private int size;
         private MultiTile[,] plane;
+        private TileSet tset;
         private MultiTile mtdefault;
 
         public MultiTilePlane()
@@ -20,6 +21,7 @@
         public MultiTilePlane(TileSet tset)
             : this()
         {
+            this.tset = tset;
             this.mtdefault = new MultiTile(tset);
         }
 
@@ -36,6 +38,52 @@
         public void Set(int x, int y, MultiTile mtile)
         {
             this.plane[x + this.size, y + this.size] = mtile;
+        }
+
+        public IEnumerable<MultiTileAction> Apply(MultiTileAction action)
+        {
+            var tile = this.Get(action.X, action.Y);
+            short original = tile.GetDirection(action.Direction);
+            short newvalue = (short)(original & action.Value);
+
+            if (newvalue == original)
+                return null;
+
+            if (newvalue == 0)
+                throw new InvalidOperationException("Contradiction");
+
+            IList<MultiTileAction> actions = new List<MultiTileAction>();
+
+            switch (action.Direction)
+            {
+                case Direction.South:
+                    actions.Add(new MultiTileAction(newvalue, Direction.North, action.X, action.Y - 1));
+                    break;
+                case Direction.North:
+                    actions.Add(new MultiTileAction(newvalue, Direction.South, action.X, action.Y + 1));
+                    break;
+                case Direction.East:
+                    actions.Add(new MultiTileAction(newvalue, Direction.West, action.X + 1, action.Y));
+                    break;
+                case Direction.West:
+                    actions.Add(new MultiTileAction(newvalue, Direction.East, action.X - 1, action.Y));
+                    break;
+            }
+
+            tile.SetDirection(action.Direction, newvalue);
+
+            var newmtile = new MultiTile(tset.SelectByColors(newvalue, action.Direction));
+
+            if (action.Direction != Direction.East)
+                actions.Add(new MultiTileAction(newmtile.East, Direction.East, action.X, action.Y));
+            if (action.Direction != Direction.North)
+                actions.Add(new MultiTileAction(newmtile.North, Direction.North, action.X, action.Y));
+            if (action.Direction != Direction.West)
+                actions.Add(new MultiTileAction(newmtile.West, Direction.West, action.X, action.Y));
+            if (action.Direction != Direction.South)
+                actions.Add(new MultiTileAction(newmtile.South, Direction.South, action.X, action.Y));
+
+            return actions;
         }
     }
 }
