d2b3854fe3416390275b98c7ca94455f7f74f96b
ajlopez <webmaster@ajlopez.com>
2016-04-09 09:48:56 -0300

    MultiTileAction creation test

---
+++ Src/WangTiles.Tests/MultiTileActionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace WangTiles.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MultiTileActionTests
+    {
+        [TestMethod]
+        public void CreateAction()
+        {
+            MultiTileAction action = new MultiTileAction(1, Direction.East, 2, 3);
+
+            Assert.AreEqual(1, action.Value);
+            Assert.AreEqual(Direction.East, action.Direction);
+            Assert.AreEqual(2, action.X);
+            Assert.AreEqual(3, action.Y);
+        }
+    }
+}
---
+++ Src/WangTiles/MultiTileAction.cs
@@ -0,0 +1,32 @@
+﻿namespace WangTiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MultiTileAction
+    {
+        private short value;
+        private int x;
+        private int y;
+        private Direction direction;
+
+        public MultiTileAction(short value, Direction direction, int x, int y)
+        {
+            this.value = value;
+            this.direction = direction;
+            this.x = x;
+            this.y = y;
+        }
+
+        public short Value { get { return this.value; } }
+
+        public Direction Direction { get { return this.direction; } }
+
+        public int X { get { return this.x; } }
+
+        public int Y { get { return this.y; } }
+    }
+}
+
