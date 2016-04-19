3a208b2fe0cb90aabbfa9938e02f44c8b04071a1
ajlopez <webmaster@ajlopez.com>
2016-04-19 06:22:22 -0300

    TilePlane Size

--- Src/WangTiles.Tests/TilePlaneTests.cs
+++ Src/WangTiles.Tests/TilePlaneTests.cs
@@ -12,6 +12,16 @@
             var plane = new TilePlane();
 
             Assert.IsTrue(plane.IsEmpty);
+            Assert.AreEqual(10, plane.Size);
+        }
+
+        [TestMethod]
+        public void SizeFive()
+        {
+            var plane = new TilePlane(5);
+
+            Assert.IsTrue(plane.IsEmpty);
+            Assert.AreEqual(5, plane.Size);
         }
     }
 }
--- Src/WangTiles/TilePlane.cs
+++ Src/WangTiles/TilePlane.cs
@@ -7,10 +7,22 @@
 
     public class TilePlane
     {
+        private int size;
+        private MultiTile[,] plane;
+
         public TilePlane()
+            : this(10)
         {
         }
 
+        public TilePlane(int size)
+        {
+            this.size = size;
+            this.plane = new MultiTile[(size * 2) + 1, (size * 2) + 1];
+        }
+
         public bool IsEmpty { get { return true; } }
+
+        public int Size { get { return this.size; } }
     }
 }
